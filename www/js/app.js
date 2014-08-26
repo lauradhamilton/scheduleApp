e50284bc643125ebcad4da1f1e48b17a6b291bf1
Laura <lauradhamilton@gmail.com>
2014-08-26 13:16:43 -0500

    Updating the home page

--- www/js/app.js
+++ www/js/app.js
@@ -47,12 +47,12 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         }
       }
     })
-    .state('app.playlists', {
-      url: "/playlists",
+    .state('app.home', {
+      url: "/home",
       views: {
         'menuContent' :{
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
+          templateUrl: "templates/home.html",
+          controller: 'HomeCtrl'
         }
       }
     })
